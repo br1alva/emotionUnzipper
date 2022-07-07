2d2eef8ba826f0563cc1ac7768560f2ce46458ae
Reddie8542 <bruno.hiram@tutanota.com>
2022-07-07 16:50:55 -0300

    Configure first experience's isProyected

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,19 +1,53 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
+import { MatButtonToggleChange } from '@angular/material/button-toggle';
+import { Subscription } from 'rxjs';
+import { Pronoun } from '../shared/models/pronoun.model';
+import { Relationship } from '../shared/models/relationship.model';
+import { FormService } from '../shared/services/form.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.component.html',
   styleUrls: ['home.component.scss'],
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
   cols = 1;
-  stepForm = this.fb.group({ step: this.fb.control(null) });
-  control = this.fb.control(null);
   linear = true;
   orientation: 'horizontal' | 'vertical' = 'horizontal';
 
-  constructor(private fb: FormBuilder) {}
+  welcomeForm = this.fb.group({
+    isProyected: this.fb.control(true),
+  });
+  ownershipOptions = [
+    { label: 'myself', value: false, tooltip: "I know it's my thing 😵‍💫..." },
+    {
+      label: 'something/someone else',
+      value: true,
+      tooltip: "Ugh 😒, it's something/someone else's fault",
+    },
+  ];
+  private sub!: Subscription;
 
-  ngOnInit(): void {}
+  constructor(private fb: FormBuilder, private forms: FormService) {}
+
+  ngOnInit(): void {
+    const relForm = this.forms.buildForm<Relationship>();
+    console.log(relForm);
+    this.sub = this.onStepFormChanges();
+  }
+
+  ngOnDestroy(): void {
+    this.sub.unsubscribe();
+  }
+
+  onFirstEventOwnershipSelection(event: MatButtonToggleChange) {
+    event.value as string;
+  }
+
+  private onStepFormChanges(): Subscription {
+    return this.welcomeForm.valueChanges.subscribe((value) =>
+      console.log(this.welcomeForm)
+    );
+  }
 }

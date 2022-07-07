1b0cdba1a0ce79b1b91bb03ce001db71fed5e257
Reddie8542 <bruno.hiram@tutanota.com>
2022-07-07 14:15:16 -0300

    Center card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,8 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatGridListModule } from '@angular/material/grid-list';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatStepperModule } from '@angular/material/stepper';
@@ -23,6 +25,8 @@ const angularModules = [
 
 const materialModules = [
   MatButtonModule,
+  MatCardModule,
+  MatGridListModule,
   MatInputModule,
   MatFormFieldModule,
   MatStepperModule,
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,11 +4,14 @@ import { FormBuilder } from '@angular/forms';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.component.html',
+  styleUrls: ['home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+  cols = 1;
   stepForm = this.fb.group({ step: this.fb.control(null) });
   control = this.fb.control(null);
   linear = true;
+  orientation: 'horizontal' | 'vertical' = 'horizontal';
 
   constructor(private fb: FormBuilder) {}
 
